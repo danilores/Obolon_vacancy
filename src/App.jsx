ce50eba621d739b7e133d80cc83daaa0d4094902
shinoki <endernasha85@gmail.com>
2023-02-11 17:39:47 +0300

    loggin \ register functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Login from './Login/Login'
+
+import Login from './pages/login/Login'
+
 function App() {
   return (
     <div className="App">
-     <Login/>
+      <Router>
+        <Routes>
+          <Route path='*' element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
